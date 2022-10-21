bdbc2eca9f4fc31dd8a50c33f9a7d31b2c556eeb
JamFox <github@jamfox.dev>
2022-10-21 10:43:25 +0300

    init: change wakeup to run ansible-update

--- roles/init/files/wakeup.sh
+++ roles/init/files/wakeup.sh
@@ -2,3 +2,13 @@
 # Managed by jamlab-ansible
 
 # Script to run on boot
+ANSIBLE_EXEC_DEST=/usr/local/sbin
+ANSIBLE_EXEC_NAME=jamlab-ansible-update
+
+if [ ! -e $ANSIBLE_EXEC_DEST/$ANSIBLE_EXEC_NAME]
+then
+    echo -e "$ANSIBLE_EXEC_DEST/$ANSIBLE_EXEC_NAME not found!" >&2
+    exit 1
+fi
+
+$ANSIBLE_EXEC_DEST/$ANSIBLE_EXEC_NAME
