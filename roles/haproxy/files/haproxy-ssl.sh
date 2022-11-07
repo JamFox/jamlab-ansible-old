7f959c1b71cd6edb84acc52f96e679b119228b46
JamFox <github@jamfox.dev>
2022-11-07 15:52:56 +0000

    use dynamic vars for hosts and host ips

--- roles/haproxy/files/haproxy-ssl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Managed by jamlab-ansible
-
-# Script to run after certbot renewal service
-
-cat /etc/letsencrypt/live/jamfox.dev/cert.pem /etc/letsencrypt/live/jamfox.dev/privkey.pem > /etc/letsencrypt/live/jamfox.dev/haproxy.pem
-systemctl restart haproxy
